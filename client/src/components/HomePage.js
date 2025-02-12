a62832e02b53a021602ee6e4ecc1358912bb60e9
Hass <hassmroueh@gmail.com>
2025-02-12 17:10:00 -0500

    Homepage styling complete

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,20 +7,20 @@ function HomePage(){
 
 
     return(
-        <div className='background'>
-        <h1 className="title">Welcome to WheelWise</h1>
-        <h2>This site is to help you track and manage vehicle ownership with ease
-        while staying organized and up-to-date with your vehicle ownership history.</h2>
-
-        <h2>Chose of the following</h2>
-         
-        <h1>Click To Browse Through Registerd Owners</h1>
+        <div>
+        <img className="hp-img" src="https://www.shutterstock.com/image-vector/car-wheel-icon-fast-speed-260nw-1218494995.jpg"/>
+        <h1 className="title">WHEEL WISE</h1>
+        <h2 className="Hp-description">Wheel Wise is a dedicated program in which allows hard working people like yourself to Browse through 
+        regestired vehicles/owners,regester a vehicle/owner and transfer ownership of a vehicle WITH EASE!!</h2>
+        
+        <h3 className="hp-p">please choose of the following:</h3>
+        <h1 className="icon-title">Click To Browse Through Registerd Owners</h1>
         <Link to='/owners'>
         <div className="icon-select-o">
         <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFZYXenNBu55mBBd5USdhyLmEzsZKtBBu1zg&s"/>
         </div>
         </Link>  
-        <h1>Click To Browse Through Registerd Vehicles</h1>
+        <h1 className="icon-title">Click To Browse Through Registerd Vehicles</h1>
         <Link to='/vehicles'>
         <div className="icon-select-v">
         <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN93WKOGeJvlcN9evkvD9gphQxl2vGjpMxGg&s"/>
