85e0e2d4b28e2336d9f0d4a3c0650b7bc9ac6a2f
Hass <hassmroueh@gmail.com>
2025-02-12 16:20:15 -0500

    Homepage styling progess

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,34 +1,36 @@
 import React from "react"
 import { useState } from "react"
 import { Link } from "react-router-dom"
-
+import './index.css'
 
 function HomePage(){
 
 
     return(
-        <>
-        <h1>Welcome to WheelWise</h1>
+        <div className='background'>
+        <h1 className="title">Welcome to WheelWise</h1>
         <h2>This site is to help you track and manage vehicle ownership with ease
         while staying organized and up-to-date with your vehicle ownership history.</h2>
 
         <h2>Chose of the following</h2>
-
+         
+        <h1>Click To Browse Through Registerd Owners</h1>
         <Link to='/owners'>
-        <>
+        <div className="icon-select-o">
         <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFZYXenNBu55mBBd5USdhyLmEzsZKtBBu1zg&s"/>
-        </>
+        </div>
         </Link>  
+        <h1>Click To Browse Through Registerd Vehicles</h1>
         <Link to='/vehicles'>
-        <>
+        <div className="icon-select-v">
         <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN93WKOGeJvlcN9evkvD9gphQxl2vGjpMxGg&s"/>
-        </>
+        </div>
         </Link>  
         
        
         
          <>  </>
-        </>
+        </div>
     )
 
 
